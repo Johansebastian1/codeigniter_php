5ab52955edd83b96741c296c985297ce24b1c15e
Johansebastian1 <johansebastiangh@gmail.com>
2014-12-03 13:46:34 -0500

    Insertando en la base de datos como debe de ser

--- application/controllers/Video.php
+++ application/controllers/Video.php
@@ -4,64 +4,39 @@ class video extends CI_controller{
 
 	public function guardar() {
 		
-		//if( isset($_COOKIE['locacion']) ){
+		if( isset($_COOKIE['locacion']) ){
+		}
+		else{
+			setcookie('locacion', 1);
+			
+			$lista =  file_get_contents('php://input');
+			$decode=json_decode($lista,true);
+			$videoId = $decode['videoId'];
+			$titulo = $decode['titulo'];
+			$url = $decode['urlimagen'];
 
-		//}
-		//else{
-			//setcookie('locacion', 1);
-			//id de la lista de reproduccion
-			//$lista = $_REQUEST['lista'];
-			//print_r($lista[0]);
-		//}
-		$videoIdStr =  file_get_contents('php://input');
-		$decode=json_decode($videoIdStr,true);
+			try {
+			    $gbd = new PDO('mysql:host=localhost;dbname=new_schema1', "root", 12345);
+			    
+			    $sql="insert into lista (id) values(1)";
 
-		echo ($decode['titulo']);//asi
-		//$videoIdStr = $this->input->POST('videoId');
-		print_r($videoIdStr);
-		// se captura el arreglo en formato cadena
-		//$listaStr = $this->input->POST('lista');
+			    $gbd->exec($sql);
 
-		//se convierte la cadena a un verdadero arreglo json
-		//$lista = json_decode( $listaStr );
-		
+			    $sql="insert into cancion (videoId, title, thumbnails, lista) values('$videoId', '$titulo', '$url', 1)";
 
-		// como para probar, se devuelve el mismo arreglo json como respuesta
-		//header('content-type: application/json');
-		//echo json_encode( $lista );
+			    $gbd->exec($sql);
 
-		//echo "asd";
-		//enviar los datos a la base de datos
-		//obtener el arreglo que tiene las canciones
-		//recorrerlo
-		//enviar los datos
-
-		//print_r(  );
-		
-		
-		
-		/**try {
-		    $gbd = new PDO('mysql:host=localhost;dbname=new_schema1', "root", 12345);
-		    
-		    $sql="insert into lista (id) values(1)";
-
-		    $gbd->exec($sql);
-
-		    echo "Ingreso a la base de datos exitoso de la lista";
-
-		    $sql="insert into cancion (lista, title, thumbnails) values(1, 'ay vamos', 'iuupiuept')";echo "Ingreso a la base de datos exitoso de la cancion";
-
-		    echo "Ingreso a la base de datos exitoso de la cancion";
-
-		    $gbd->exec($sql);
-		    foreach($gbd->query('SELECT * from FOO') as $fila) {
-		        print_r($fila);
-		    }
-		    $gbd = null;
-		   
-		} catch (PDOException $e) {
-		    print "¡Error!: " . $e->getMessage() . "<br/>";
-		    die();
-		}**/
+			    echo "Ingreso a la base de datos exitoso de la cancion";
+			  
+			  	/**foreach($gbd->query('SELECT * from FOO') as $fila) {
+			        print_r($fila);
+			    }**/
+			    $gbd = null;
+			   
+			} catch (PDOException $e) {
+			    print "¡Error!: " . $e->getMessage() . "<br/>";
+			    die();
+			}
+		}
 	}
 }
